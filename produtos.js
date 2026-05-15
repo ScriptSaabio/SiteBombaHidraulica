b488f4aaff8b30e88d922d5ca1a01fa386a1d3fc
Douglas <92474825+ScriptSaabio@users.noreply.github.com>
2026-05-14 21:50:34 -0300

    atualizações

---
+++ produtos.js
@@ -0,0 +1,27 @@
+export let produtos = [
+
+    {
+        id: 1,
+        nome: "Alta Pressão",
+        descricao: "Ideal para prédios e sistemas que exigem força total.",
+        imagem: "img/1.jpg",
+        preco: 1200
+    },
+
+    {
+        id: 2,
+        nome: "Submersíveis",
+        descricao: "Bombas projetadas para trabalhar dentro da água.",
+        imagem: "img/2.jpg",
+        preco: 1200
+    },
+
+    {
+        id: 3,
+        nome: "Manutenção",
+        descricao: "Suporte técnico especializado para todos os modelos.",
+        imagem: "img/3.jpg",
+        preco: 1200
+    }
+
+];
---
+++ usuarios.js
@@ -0,0 +1,5 @@
+export let usuarios = [
+  { id: 1, usuario: "admin@admin.com", senha: '123'},
+  { id: 2, usuario: "user@user.com", senha: '123' },
+  { id: 3, usuario: "tester@tester.com", senha: '123' }
+];
